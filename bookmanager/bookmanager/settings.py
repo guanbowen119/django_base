7c60e6e9102113f800b1df2faabd61e00a4ef71e
guanbowen119 <1467044722@qq.com>
2024-02-11 00:26:05 +0800

    静态资源设置和访问

--- bookmanager/bookmanager/settings.py
+++ bookmanager/bookmanager/settings.py
@@ -120,3 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# 告知系统我们的静态资源在哪里
+STATICFILES_DIRS=[
+    os.path.join(BASE_DIR, 'static')
+]
